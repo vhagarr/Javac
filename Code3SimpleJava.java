82d0bac6142f0bdea2c985337daa154c414f89fc
vhagarr <ranjitbhichukale11@gmail.com>
2024-10-17 15:12:55 +0530

    Create Code3SimpleJava.java

---
+++ Code3SimpleJava.java
@@ -0,0 +1,41 @@
+//reverse the number
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
+
+//prime Number
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers from 1 to 100:");
+
+        for (int number = 2; number <= 100; number++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+}
