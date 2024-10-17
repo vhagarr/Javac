aa4be513ca05cdfc18c22beb18f487dfe0f44121
vhagarr <ranjitbhichukale11@gmail.com>
2024-10-17 15:58:07 +0530

    Update Code3SimpleJava.java

--- Code3SimpleJava.java
+++ Code3SimpleJava.java
@@ -1,7 +1,7 @@
 //reverse the number
 import java.util.Scanner;
 
-public class ReverseNumber {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
@@ -19,7 +19,7 @@ public class ReverseNumber {
 }
 
 //prime Number
-public class PrimeNumbers {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Prime numbers from 1 to 100:");
 
