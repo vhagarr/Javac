c9b3984f379adc64c1cda9bc613850cdd94710a8
vhagarr <ranjitbhichukale11@gmail.com>
2024-10-17 15:59:18 +0530

    Update Code6EvenPlaceArray.java

--- Code6EvenPlaceArray.java
+++ Code6EvenPlaceArray.java
@@ -1,5 +1,5 @@
 //EvenPlace in Array
-public class EvenPositionElements {
+public class Main {
     public static void main(String[] args) {
         int[] array = {10, 20, 30, 40, 50, 60, 70};
         printEvenPositionElements(array);
