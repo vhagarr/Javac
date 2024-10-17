ce7f2722bb5089f503659598d71df91917b2dab8
vhagarr <ranjitbhichukale11@gmail.com>
2024-10-17 15:22:20 +0530

    Create Code6EvenPlaceArray.java

---
+++ Code6EvenPlaceArray.java
@@ -0,0 +1,17 @@
+//EvenPlace in Array
+public class EvenPositionElements {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50, 60, 70};
+        printEvenPositionElements(array);
+    }
+
+    public static void printEvenPositionElements(int[] arr) {
+        System.out.println("Elements at even positions:");
+        for (int i = 0; i < arr.length; i++) {
+            // Check if the index is even
+            if (i % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
