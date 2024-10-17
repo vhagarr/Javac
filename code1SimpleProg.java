c95ce2ef0fb260ee03087eb2f7c49eb13f0ef172
vhagarr <ranjitbhichukale11@gmail.com>
2024-10-17 15:05:58 +0530

    Create code1SimpleProg.java

---
+++ code1SimpleProg.java
@@ -0,0 +1,40 @@
+//Fibonacci
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms in the Fibonacci series: ");
+        int terms = scanner.nextInt();
+
+        int first = 0, second = 1;
+
+        System.out.print("Fibonacci Series: ");
+        for (int i = 1; i <= terms; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
+
+//Factprial
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to find its factorial: ");
+        int number = scanner.nextInt();
+
+        long factorial = 1;
+
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+}
+
