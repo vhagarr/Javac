7b4946c8c6a56c82a6baf64a543ba190f03f74ef
vhagarr <ranjitbhichukale11@gmail.com>
2024-10-17 15:57:32 +0530

    Update Code2SimpleJava.java

--- Code2SimpleJava.java
+++ Code2SimpleJava.java
@@ -1,7 +1,7 @@
 //Armstrong
 import java.util.Scanner;
 
-public class ArmstrongNumber {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
@@ -32,7 +32,7 @@ public class ArmstrongNumber {
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class AnagramCheck {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
